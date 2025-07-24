28d2f99107ad0200928f6a563e482a61f067f054
ai7287 <ai72877@naver.com>
2025-07-24 17:31:10 +0900

    pbl 네트워크보안 2주차 1번문제

--- shieldus_pbl/pbl5/test.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-# 명시적으로 상위 폴더의 .env 경로 지정
-load_dotenv(dotenv_path='c:/skshieldus26th/.env')
-
-print("ID:", os.getenv("SECRET_ID"))
-print("PASS:", os.getenv("SECRET_PASS"))
-    
